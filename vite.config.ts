a884e851afdd5b04b2b17482255c4c41d173879b
King <max.musternass@outlook.de>
2025-07-26 00:41:01 +0200

    Konfiguriere Tailwind CSS und Firebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
+   plugins: [react(), tailwindcss()],
     const env = loadEnv(mode, '.', '');
     return {
       define: {
